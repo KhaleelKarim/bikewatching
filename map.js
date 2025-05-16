d82bc7731b212c213278c3aa976f6cb99f666eb9
Khaleel Karim <kkarim@ucsd.edu>
2025-05-16 15:03:32 -0700

    Map is here!@!!!!

--- map.js
+++ map.js
@@ -1,5 +1,74 @@
 // Import Mapbox as an ESM module
 import mapboxgl from 'https://cdn.jsdelivr.net/npm/mapbox-gl@2.15.0/+esm';
+import * as d3 from 'https://cdn.jsdelivr.net/npm/d3@7.9.0/+esm';
 
 // Check that Mapbox GL JS is loaded
 console.log('Mapbox GL JS Loaded:', mapboxgl);
+
+mapboxgl.accessToken = 'pk.eyJ1Ijoia2hhbGVlbGthcmltIiwiYSI6ImNtYXFib3F3bzA4OGkybG9sOTUybmFqeHMifQ.uF_KaxtT3R-RcpuNklWCPQ';
+
+
+// Initialize the map
+const map = new mapboxgl.Map({
+  container: 'map', // ID of the div where the map will render
+  style: 'mapbox://styles/mapbox/streets-v12', // Map style
+  center: [-71.09415, 42.36027], // [longitude, latitude]
+  zoom: 12, // Initial zoom level
+  minZoom: 5, // Minimum allowed zoom
+  maxZoom: 18, // Maximum allowed zoom
+});
+
+// Load in geojson stuff
+map.on('load', async () => {
+    // Boston bike lanes
+    map.addSource('boston_route', {
+    type: 'geojson',
+    data: 'https://bostonopendata-boston.opendata.arcgis.com/datasets/boston::existing-bike-network-2022.geojson',
+    });
+
+    map.addLayer({
+        id: 'bike-lanes-boston',
+        type: 'line',
+        source: 'boston_route',
+        paint: {
+            'line-color': 'green',
+            'line-width': 3,
+            'line-opacity': 0.4,
+        },
+    });
+
+    // Cambridge bike lanes
+    map.addSource('cambridge_route', {
+    type: 'geojson',
+    data: 'https://raw.githubusercontent.com/cambridgegis/cambridgegis_data/main/Recreation/Bike_Facilities/RECREATION_BikeFacilities.geojson',
+    });
+
+    map.addLayer({
+        id: 'bike-lanes-cambridge',
+        type: 'line',
+        source: 'cambridge_route',
+        paint: {
+            'line-color': 'red',
+            'line-width': 3,
+            'line-opacity': 0.4,
+        },
+    });
+
+    // Bike stations
+    let jsonData;
+    try {
+        const jsonurl = "/bluebikes-stations.json";
+
+        // Await JSON fetch
+        jsonData = await d3.json(jsonurl);
+
+        console.log('Loaded JSON Data:', jsonData); // Log to verify structure
+    } catch (error) {
+        console.error('Error loading JSON:', error); // Handle errors
+    }
+
+    // Proof we got da stations
+    let stations = jsonData.data.stations;
+    console.log('Stations Array:', stations);
+
+});
